aade7868f3287b79399df6e8bbb6195a5f81265e
JoaoACastro <joaoaccastro@gmail.com>
2022-04-08 22:52:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+import Conversor from './components/Conversor';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className='titulo'>.:Conversor de Moedas:.</h1>
+        <div className="index">
+          <div className="legendaContainer">
+            <div className="legenda">
+              <h3 className='legendaTitulo'>Legenda:</h3>
+              <p>BRL: Real brasileiro</p>
+              <p>USD: Dólar Americano</p>
+              <p>EUR: Euro</p>
+              <p>CAD: Dólar Canadense</p>
+              <p>ARS: Peso Argentino</p>
+            </div>
+          </div>
+
+          <div className="conversores">
+            <div className="linha">
+              <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+              <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+            </div>
+            <div className="linha">
+              <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+              <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+            </div>
+            <div className="linha">
+              <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+              <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+            </div>
+            <div className="linha">
+              <Conversor moedaA="ARS" moedaB="BRL"></Conversor>
+              <Conversor moedaA="BRL" moedaB="ARS"></Conversor>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
