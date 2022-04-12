762c37e655c44b56e7529c048d7f8b415acfa3dd
JoaoACastro <joaoaccastro@gmail.com>
2022-04-12 17:52:51 -0300

    Merge branch 'master' of https://github.com/joaoacastro/conversor_de_moedas_react

--- src/App.js
+++ src/App.js
@@ -22,20 +22,20 @@ class App extends Component {
 
           <div className="conversores">
             <div className="linha">
-              <Conversor moedaA="USD" moedaB="BRL"></Conversor>
               <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+              <Conversor moedaA="USD" moedaB="BRL"></Conversor>
             </div>
             <div className="linha">
-              <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
               <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+              <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
             </div>
             <div className="linha">
-              <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
               <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+              <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
             </div>
             <div className="linha">
-              <Conversor moedaA="ARS" moedaB="BRL"></Conversor>
               <Conversor moedaA="BRL" moedaB="ARS"></Conversor>
+              <Conversor moedaA="ARS" moedaB="BRL"></Conversor>
             </div>
           </div>
         </div>
