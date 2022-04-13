71c5396e082cdf6c6f7588a8ac927efa36817a60
JoaoACastro <joaoaccastro@gmail.com>
2022-04-13 11:50:56 -0300

    master

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -15,7 +15,7 @@ export default class Conversor extends Component {
 
     converter(){
         let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
-        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=b1460e287d696041ccb1`        
+        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=b1460e287d696041ccb1`     
 
         fetch(url)
         .then(res=>{
@@ -34,7 +34,7 @@ export default class Conversor extends Component {
         return(
             <div className="conversor">
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-                <input type="text" onChange={(e) => {this.setState({moedaA_valor: e.target.value }); }}/>
+                <input type="text" id="entrada" onChange={(e) => {this.setState({moedaA_valor: e.target.value }); }}/>
                 <input type="button" className="button" value="Converter" onClick={this.converter}/>
                 <h2>{this.state.moedaB_valor}</h2>
             </div>
